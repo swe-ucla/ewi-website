4a18984a67ac22e2dbb7f62c7f1531f7ccdb33b9
impana-c <impana@gmail.com>
2023-08-31 09:59:35 -0700

    Updated the homepage based on Figma and director requests.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,39 +11,16 @@ const About = () => {
   return (
     <section id='about'>
       <div className="container about__content"> 
-            <h2><p>About Us</p></h2>
+            <h2>About Us</h2>
             <div className="about__paragraph">
             <h4> 
-            <p> Evening with Industry (EWI) is one of Society of Women Engineers’ flagship events and the largest student-run networking event on UCLA campus. <br /></p>
-            <p> In 2024, SWE @ UCLA will host the 47th annual Evening with Industry at UCLA’s Carnesale Commons. <br /></p>
-            <p> At this event, we strive to create an environment that is more personal than a typical career fair and foster more genuine connections as a result. <br /></p>
-            <p> To do this, our event has a three part structure: informal networking over appetizers, a dinner, and then a formal career fair. <br /></p>
-            <p> During dinner, students get to sit with and talk to representatives from the company of their choice. The relaxed and informal dinner setting contrasts a high-stakes, fast-paced career fair, encouraging more unscripted conversations and allowing both parties to engage in more personal conversation. There is more time for students and representatives to present themselves authentically and connect over shared experiences. </p>
-            
+            <p>Evening with Industry (EWI) is one of Society of Women Engineers’ flagship events and the largest student-run networking event on UCLA campus. In 2024, SWE @ UCLA will host the 47th annual Evening with Industry at UCLA’s Carnesale Commons. <br/></p>
+            <p>At this event, we strive to create an environment that is more personal than a typical career fair. To do this, our event has a three-part structure: informal networking over appetizers, a multi-course catered dinner, and then a formal career fair. <br/></p>
+            <p>During dinner, students get to sit with and talk to representatives from the company of their choice. The relaxed and informal dinner setting contrasts a high-stakes, fast-paced career fair, encouraging more unscripted conversations and allowing both parties to engage in more genuine conversation. The extended time and decreased pressure allows students and representatives to present themselves authentically and connect over shared experiences. <br/></p>
+            <p>While the event is traditionally oriented towards women in engineering, <b>we welcome students of all genders and academic backgrounds</b> relevant to the engineering field. Our primary goal is to ensure that every attendee gains an enriching and worthwhile experience. </p>
             </h4>
          </div>
     </div>
-
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={SWEPHOTO} alt="About Image" />
-          </div>
-        </div>
-
-        <div className="about__content">
-          <div className="about__cards">
-
-            <article className='about__card'>
-              <h2>Our goals</h2>
-              <h4>While the event is traditionally oriented towards women in engineering, we welcome students of all genders and academic backgrounds relevant to the engineering field. Our primary goal is to ensure that every attendee gains an enriching and worthwhile experience.<br /><br />
-              <b>Try to meet someone different than you?</b>
-              </h4>
-            </article>
-          </div>
-          
-        </div>
-      </div>
       <h2><br/>Quote here</h2>
     </section>
   )
