e5582e9803850586efee36b934847211736a00a2
impana-c <impana@gmail.com>
2023-09-26 11:32:53 -0700

    Resolved merge conflict.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,36 +10,6 @@ import {HiOutlineLightBulb} from 'react-icons/hi'
 const About = () => {
   return (
     <section id='about'>
-<<<<<<< HEAD
-      <h2>About Us</h2>
-
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={SWEPHOTO} alt="About Image" />
-          </div>
-        </div>
-
-        <div className="about__content">
-          <div className="about__cards">
-
-            <article className='about__card'>
-              <h2>Our goals</h2>
-              <h4>Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-
-Integer vitae justo eget magna fermentum iaculis. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Eget felis eget nunc lobortis mattis aliquam faucibus. Elit at imperdiet dui accumsan. Lectus mauris ultrices eros in cursus. Sagittis eu volutpat odio facilisis mauris sit amet. Libero nunc consequat interdum varius sit amet mattis vulputate. Lectus arcu bibendum at varius vel pharetra vel turpis. Diam maecenas sed enim ut sem. Sed euismod nisi porta lorem. 
-
-</h4>
-            </article>
-          </div>
-          
-        </div>
-      </div>
-      <h2>Quote here</h2>
-      
-
-         <CTAabout />
-=======
       <div className="container about__content"> 
             <h2>About Us</h2>
             <div className="about__paragraph">
@@ -52,7 +22,6 @@ Integer vitae justo eget magna fermentum iaculis. Hac habitasse platea dictumst 
          </div>
     </div>
       <h2><br/>Quote here</h2>
->>>>>>> impana_branch
     </section>
   )
 }
