34588864dfd208822a3905c88eb6362d0c0fcb94
Emily Nham <emilynham@ucla.edu>
2023-10-08 14:04:49 -0700

    Add Company page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../components/header/Header'
+import Nav from '../components/nav/Nav'
+import About from '../components/about/About'
+import Attend from '../components/attend/attend'
+import Navbar from '../components/navbar/navbar'
+import Portfolio from '../components/portfolio/Portfolio'
+import Contact from '../components/contact/Contact'
+import Footer from '../components/footer/Footer'
+
+const Home = () => {
+  return (
+        <div className='home'>
+            {/* <Navbar /> */}
+            <Header />
+            <Attend />
+            <Nav />
+            <About />
+            <Portfolio />
+            <Contact />
+            {/* <Footer /> */}
+        </div>
+  )
+}
+
+export default Home
