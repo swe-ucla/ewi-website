b86cbeb919143aa3a3e8e77e2417e5e8ef75d4fe
cloyooni <146996021+cloyooni@users.noreply.github.com>
2024-11-14 18:47:36 -0800

    Updated all fonts and sizes for company header and info

--- src/components/companyHeader/CompanyHeader.jsx
+++ src/components/companyHeader/CompanyHeader.jsx
@@ -7,9 +7,6 @@ const CompanyHeader = () => {
     <div className="cheader">
       <div className="container cheader__container">
         <div className="cheaderText">
-        <h2>
-          <span className="f">f</span>or companie<span className="s">s</span><br/>
-        </h2>
         <span className="f">f</span>
           <h1>
             <span className="middle">or companie<span className="s">s</span></span>
