72df9ab39f7949061a3ba40225a184ad22fc33ab
anyachen27 <150403358+anyachen27@users.noreply.github.com>
2026-01-11 23:10:21 -0800

    Update event details and styling in EventsSection component; enhance layout and improve card presentation

--- src/components/eventsSection/EventsSection.tsx
+++ src/components/eventsSection/EventsSection.tsx
@@ -39,30 +39,36 @@ const eventsList: EventData[] = [
   {
     number: 1,
     rsvpLink: "google.com",
-    title: "Event 1",
-    dateTime: "Date Time",
-    description: "This is the description",
+    title: "Connections that Count: Networking with EWI",
+    dateTime: "Tuesday, Jan. 6th 6-7pm",
+    description:
+      "Learn how to start conversations, build genuine connections, and network with confidence at events like EWI.",
   },
   {
     number: 2,
     rsvpLink: "google.com",
-    title: "Event 1",
-    dateTime: "Date Time",
-    description: "This is the description",
+    title: "LockedIn with LinkedIn",
+    dateTime: "Thursday, Jan. 8th 5-6pm",
+    description:
+      "Turn your LinkedIn profile into a professional first impression that recruiters are eager to click on.",
   },
   {
     number: 3,
-    rsvpLink: "google.com",
-    title: "Event 1",
-    dateTime: "Date Time",
-    description: "This is the description",
+    rsvpLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSdq1k42ZOhhAAlFQkY0tG6wQpZHrPrOlZpGfiQ1HnfFwUG5XQ/viewform?usp=header",
+    title: "Perfect your Pitch",
+    dateTime: "Tuesday, Jan. 13th 6-7pm",
+    description:
+      "Nervous about talking to over 20 recruiters at Evening with Industry 2026 📝? Level up your elevator pitch at our third EWI preparational workshop on Tuesday, Jan. 13 📈 Join us at EV 2101 from 6-7 pm to craft and practice a clear, confident elevator pitch that highlights your strengths and skills for your next internship or full-time job  🎉",
   },
   {
     number: 4,
-    rsvpLink: "google.com",
-    title: "Event 1",
-    dateTime: "Date Time",
-    description: "This is the description",
+    rsvpLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLScFx3EnVtKuiEI7lU3oNC_OPAk9Z4dyt4nPzT0HqSnVOHrhiA/viewform?usp=header",
+    title: "Resume Ready",
+    dateTime: "Thursday, Jan. 15th 6-7pm",
+    description:
+      "Update and strengthen your resume with actionable tips to make it recruiter-ready for internship applications",
   },
 ];
 
