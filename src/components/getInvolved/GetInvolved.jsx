fb3929ed4c07ddb5bc20b7dcaeeeb1676d822b4c
anyachen27 <150403358+anyachen27@users.noreply.github.com>
2026-01-11 22:55:01 -0800

    Revert non-homepage changes: restore company/student page components to match origin/master

--- src/components/getInvolved/GetInvolved.jsx
+++ src/components/getInvolved/GetInvolved.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './GetInvolved.css';
 
 const GetInvolved = () => {
@@ -7,28 +8,28 @@ const GetInvolved = () => {
       <div className="get-involved-content">
         <h2 className="get-involved-title">get involved</h2>
         <p className="get-involved-description">
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+          Platinum and Gold level sponsors of SWE UCLA are able to register for EWI for free, in addition to other benefits. Registration includes one dinner table and one career fair booth for up to two representatives, providing an intimate setting to connect with top engineering talent. If a partnership with SWE UCLA is of interest, please contact <a href="mailto:ewi.swe.ucla@gmail.com" style={{textDecoration: 'underline'}}>ewi.swe.ucla@gmail.com</a> to learn more about sponsorship opportunities and how to get involved.
         </p>
         <div className="get-involved-buttons">
-          <button className="involved-button">SPONSOR US</button>
-          <button className="involved-button">ABOUT EWI</button>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSd0HSR3w96uyGjYzzJEKVf-VeGeaTtDT-yg6DKei4dNx9cI-Q/viewform" target="_blank" rel="noopener noreferrer" className="involved-button">SPONSOR US</a>
+          <Link to="/" className="involved-button">ABOUT EWI</Link>
         </div>
       </div>
       <div className="get-involved-stats">
         <div className="stat-circle">
           <div className="stat-circle-bg"></div>
-          <p className="stat-number">00+</p>
-          <p className="stat-label">participants</p>
+          <p className="stat-number">49</p>
+          <p className="stat-label">years running</p>
         </div>
         <div className="stat-circle">
           <div className="stat-circle-bg"></div>
           <p className="stat-number">00+</p>
-          <p className="stat-label">participants</p>
+          <p className="stat-label">students</p>
         </div>
         <div className="stat-circle">
           <div className="stat-circle-bg"></div>
           <p className="stat-number">00+</p>
-          <p className="stat-label">participants</p>
+          <p className="stat-label">companies</p>
         </div>
       </div>
     </div>
--- src/components/studentHeader/StudentHeader.jsx
+++ src/components/studentHeader/StudentHeader.jsx
@@ -7,9 +7,12 @@ const StudentHeader = () => {
     <div className="student-header">
       <div className="student-header-content">
         <img src={student_header_text} className="student-text" alt="Student Registration text" />
-        <h3 className="h3">regular deadline by 0/0</h3>
-        <button className="button">APPLY</button>
-        <p className="p">Eligibility: Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+        <h3 className="h3">regular deadline by 1/16</h3>
+        <a className="button-link" href="https://docs.google.com/forms/d/1PfdlBL6_Ktjdf8y0rcf4T4torBHp0mVE85wQdN2TaOs/viewform?edit_requested=true" target="_blank" rel="noopener noreferrer">
+          REGISTER
+        </a>
+        <p className="p">Registration is first come, first serve. The earlier you sign up, the more likely you will be to be seated with your top companies during dinner. This event is open to everyone regardless of gender. We will begin a waitlist as we hit capacity. </p>
+        <p className="p">EWI is cost-free this year! Registration is first-come-first-serve, so make sure to submit the form as soon as possible.</p>
       </div>
     </div>
   )
